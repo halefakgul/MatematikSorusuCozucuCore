952acb7055f1388d8a25dad0916a6398ffbfe7a6
Halef Akgul <89591208+halefakgul@users.noreply.github.com>
2021-08-28 17:30:50 +0300

    PrimaryExpression Calculate have been added

---
+++ src/main/java/com/halefakgul/Expressions/PrimaryExpression/Calculate.java
@@ -0,0 +1,12 @@
+package com.halefakgul.Expressions.PrimaryExpression;
+
+public class Calculate extends BasePrimaryExpression{
+
+    public Calculate(String[] exps) {
+        super(exps);
+    }
+
+    public Double getResult() {
+        return 0d + 5d;
+    }
+}
