9f181facb3da487fc2cbeee007c66e43fc96e010
Halef Akgul <89591208+halefakgul@users.noreply.github.com>
2021-08-28 17:32:39 +0300

    Now app defines the 1.arg of input as an expression object and solves input data

--- src/main/java/com/halefakgul/MatematikSorusuCozucuCore.java
+++ src/main/java/com/halefakgul/MatematikSorusuCozucuCore.java
@@ -1,9 +1,28 @@
 package com.halefakgul;
 
-import java.util.Arrays;
+import com.halefakgul.Expressions.Expression;
+import com.halefakgul.Expressions.PrimaryExpression.Calculate;
 
 public class MatematikSorusuCozucuCore {
-    public static void main(String[] args) {
+    private static String primaryExpression;
+    private static Object primaryExp;
 
+    public static void main(String[] args) {
+        if (args == null ||args.length == 0){
+            return;
+        }
+        primaryExpression = args[0];
+        String[] otherExpressions = new String[args.length - 1];
+        for (int i = 1; i < otherExpressions.length; i++){
+            otherExpressions[i] = args[i];
+        }
+
+        switch(primaryExpression){
+            case "cal":
+                primaryExp = new Calculate(otherExpressions);
+                break;
+        }
+
+        System.out.println(((Expression) primaryExp).getResult());
     }
 }
