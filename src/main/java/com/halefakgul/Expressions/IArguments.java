01a9cb07c50ab2feff4704015a76eb3405075a01
Halef Akgul <89591208+halefakgul@users.noreply.github.com>
2021-08-29 22:00:01 +0300

    IArguments interface have been added to define expressions which has arguments.

---
+++ src/main/java/com/halefakgul/Expressions/IArguments.java
@@ -0,0 +1,8 @@
+package com.halefakgul.Expressions;
+
+import java.util.ArrayList;
+
+public interface IArguments {
+    public void setArguments(String ... arguments);
+    public void setArguments(ArrayList<String> arguments);
+}
