77c4b6239b4f1789376fde7f2d737a62c63d6f3d
Halef Akgul <89591208+halefakgul@users.noreply.github.com>
2021-09-09 21:48:24 +0300

    Messages are gathered to single class

---
+++ src/main/java/com/halefakgul/Messager/Messages.java
@@ -0,0 +1,11 @@
+package com.halefakgul.Messager;
+
+public enum Messages {
+    fileNotFoundMessage("An input required. (Ex: compile test.msc)"),
+    ioExceptionMessage("IOException has occoured while file is exporting");
+    protected String message;
+
+    Messages(String message) {
+        this.message = message;
+    }
+}
