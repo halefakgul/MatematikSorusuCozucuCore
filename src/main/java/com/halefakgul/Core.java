a514e15f4685d0219afbf4d1c7dded341323d686
Halef Akgul <89591208+halefakgul@users.noreply.github.com>
2021-09-20 23:02:04 +0300

    New expressions defined at the inside Core for now

--- src/main/java/com/halefakgul/Core.java
+++ src/main/java/com/halefakgul/Core.java
@@ -1,5 +1,11 @@
 package com.halefakgul;
 
+import com.halefakgul.Expressions.BaseExpressions.UndefinedExp;
+import com.halefakgul.Expressions.BaseExpressions.Print;
+import com.halefakgul.Expressions.BaseExpressions.Solve;
+import com.halefakgul.Expressions.ExpressionManager;
+import com.halefakgul.Expressions.MathExpressions.Pow;
+import com.halefakgul.Expressions.MathExpressions.Square;
 import com.halefakgul.Functions.Compile;
 import com.halefakgul.Functions.Execute;
 import com.halefakgul.Functions.FunctionManager;
@@ -8,13 +14,23 @@ import com.halefakgul.Messager.MessageManager;
 public class Core {
     public static String function;
     public static String[] arguments;
-    public MessageManager messager = new MessageManager();
-    public FunctionManager functions = new FunctionManager();
-
+    public MessageManager messager;
+    public FunctionManager functions;
+    public ExpressionManager expressions;
 
     private String[] args;
     public Core(String[] args){
         this.args = args;
+
+        messager = new MessageManager();
+        functions = new FunctionManager();
+        expressions = new ExpressionManager();
+
+        expressions.add(new Square());
+        expressions.add(new Pow());
+        expressions.add(new Print());
+        expressions.add(new Solve(new UndefinedExp()));
+
 
         function = args[0];
         arguments = new String[args.length - 1];
