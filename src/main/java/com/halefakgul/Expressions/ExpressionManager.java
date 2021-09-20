3d3aa849147ca1a0fc5f8f9228780c5db632edfe
Halef Akgul <89591208+halefakgul@users.noreply.github.com>
2021-09-20 23:04:57 +0300

    Upgraded to work with simplified project

--- src/main/java/com/halefakgul/Expressions/ExpressionManager.java
+++ src/main/java/com/halefakgul/Expressions/ExpressionManager.java
@@ -1,24 +1,22 @@
 package com.halefakgul.Expressions;
 
-import com.halefakgul.Expressions.BasicMathExpressions.Square;
 
 import java.util.ArrayList;
 
 public class ExpressionManager {
-    private ArrayList<IExpression> expressions;
+    private ArrayList<IExpressionBase> expressions;
 
     public ExpressionManager(){
         expressions = new ArrayList<>();
-        expressions.add(new Square());
     }
 
-    public void add(IExpression type){
+    public void add(IExpressionBase type){
         this.expressions.add(type);
     }
 
 
-    public IExpression getExpression(String arg){
-        for (IExpression exp : expressions){
+    public IExpressionBase getExpression(String arg){
+        for (IExpressionBase exp : expressions){
             if (exp instanceof ICommand){
                 if (((ICommand) exp).hasCommand(arg)){
                     return exp;
@@ -31,7 +29,45 @@ public class ExpressionManager {
     public Object getExpressionManager(){
         return this;
     }
-    public ArrayList<IExpression> getExpressions(){
+    public ArrayList<IExpressionBase> getExpressions(){
         return this.expressions;
     }
+
+
+
+    public String getCode(IExpressionBase exp){
+        String code = exp.getCode();
+        String allArgs = "";
+        ArrayList<String> args = exp.getArguments();
+        for (int i = 0; i < args.size(); i++){
+            allArgs += args.get(i);
+            if (i != args.size() - 1){
+                allArgs += ", ";
+            }
+        }
+        return code.replace("$args", allArgs);
+    }
+/*
+    public String split(String readedExp){
+        ArrayList<String> args = new ArrayList<>();
+        args.add("");
+        int counter = 0;
+        for (char c : readedExp.toCharArray()){
+            switch (c){
+                case '(':
+                    args.add("");
+                    counter++;
+                    break;
+                case ')':
+                    counter--;
+                    break;
+                default:
+                    args.set(counter, args.get(counter) + c);
+            }
+        }
+    }
+
+    public String solve(String toSolve){
+        IExpressionBase exp = getExpression(toSolve); //argumanları nasıl ayıklayacak
+    }*/
 }
